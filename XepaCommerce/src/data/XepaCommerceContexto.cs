9cb13592068d84761ac81996f67c68e4e6161255
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-05-04 15:43:31 -0300

    @Lucas-Reluz [Task: Criando contexto] {04/05/2022 15:43}

--- XepaCommerce/src/data/XepaCommerceContexto.cs
+++ XepaCommerce/src/data/XepaCommerceContexto.cs
@@ -1,7 +1,17 @@
-namespace .
+using Microsoft.EntityFrameworkCore;
+using XepaCommerce.src.modelos;
+
+namespace XepaCommerce.src.data
 {
-    public class XepaCommerceContexto
+    public class XepaCommerceContexto : DbContext
     {
-        
+        public DbSet<UsuarioModelo> Usuarios { get; set; }
+        public DbSet<ProdutoModelo> Produtos { get; set; }
+        public DbSet<PedidoModelo> Pedidos { get; set; }
+
+        public XepaCommerceContexto(DbContextOptions<XepaCommerceContexto> opt) : base(opt)
+        {
+
+        }
     }
 }
