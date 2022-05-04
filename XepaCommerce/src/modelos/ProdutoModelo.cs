a07b414bb672f0202ca5a0a4d9e8e49724de9361
vntsmatos <ovinidev@gmail.com>
2022-05-04 15:30:16 -0300

    @vntsmatos [Task: Criacao ProdutoModelo] {04/05/2022 | 15:30}

---
+++ XepaCommerce/src/modelos/ProdutoModelo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace XepaCommerce.src.modelos
+{
+    public class ProdutoModelo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string NomeProduto { get; set; }
+
+        [Required]
+        public float Preco { get; set; }    
+
+        [StringLength (100)]
+        public string Descricao { get; set; }
+
+        [Required, StringLength(100)]
+        public string Foto { get; set; }  
+
+        [Required]
+        public int Estoque { get; set; }    
+
+        //[JsonIgnore]
+        //public List<ProdutoModelo> RelatedPosts { get; set; }
+        
+    }
+}
