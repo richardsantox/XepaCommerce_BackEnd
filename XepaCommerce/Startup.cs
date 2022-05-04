c1b879a141f822fa66a5aa40cf554d5f3a014124
matheuscms95 <matheuscms95@gmail.com>
2022-05-04 15:46:40 -0300

    @matheuscms95 [Task: Criando metodo configure] {04/05/2022 15:46}

--- XepaCommerce/Startup.cs
+++ XepaCommerce/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +18,16 @@ namespace XepaCommerce
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Contexto
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            services.AddDbContext<XepaCommerceContexto>(opt => opt.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            //Configuração controlador            
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
