57a53a537f85c2314550db0818dadc1b030be749
paulabrufato <paulabrufato.pb@gmail.com>
2022-05-04 17:28:04 -0300

    @paulabrufato [Update XepaCommerceContexto] {04/05/2022 17:27}

--- XepaCommerce/src/data/XepaCommerceContexto.cs
+++ XepaCommerce/src/data/XepaCommerceContexto.cs
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 using Microsoft.EntityFrameworkCore;
 using XepaCommerce.src.modelos;
 
->>>>>>> d5bdb2fbcbae14d0c6496b9cea1d4ff5b1515540
 namespace XepaCommerce.src.data
 {
     public class XepaCommerceContexto : DbContext
