0ef11e43b6cee118b247101fe4bd67b3f02655c2
danny-for <dannygeovanna2019@gmail.com>
2022-05-05 11:06:31 -0300

    @Danny-for[Task: Criando UsuarioDTO] {05/05/2022 11:05}

--- XepaCommerce/src/dtos/UsuarioDTO.cs
+++ XepaCommerce/src/dtos/UsuarioDTO.cs
@@ -1,6 +1,51 @@
-﻿namespace XepaCommerce.src.dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XepaCommerce.src.dtos
 {
-    public class UsuarioDTO
+    public class NovoUsuarioDTO
     {
+        [Required , StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required ,StringLength(30)]
+        public string Email { get; set; }
+
+        [Required, StringLength(30)]
+        public string Senha { get; set; }
+
+        [Required]
+        public string Endereco { get; set; }
+    
+        
+        public NovoUsuarioDTO(string nome ,string email , string senha , string endereco )
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Endereco = endereco;
+        }
+    }
+
+    public class AtualizarUsuarioDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required, StringLength(30)]
+        public string Senha { get; set; }
+
+        [Required]
+        public string Endereco { get; set; }
+
+        public AtualizarUsuarioDTO( string nome , string senha , string endereco )
+        {
+            Nome = nome;
+            Senha = senha;
+            Endereco = endereco;
+
+        }
     }
 }
