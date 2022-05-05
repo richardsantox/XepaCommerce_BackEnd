2533eb4b27c4c80da49f9e0015c326404aa2a89a
vntsmatos <ovinidev@gmail.com>
2022-05-05 10:58:02 -0300

    @vntsmatos [Criacao Interface Usuario] {05/05/2022 | 10:58}

--- XepaCommerce/src/repositorios/IUsuario.cs
+++ XepaCommerce/src/repositorios/IUsuario.cs
@@ -1,6 +1,20 @@
-﻿namespace XepaCommerce.src.repositorios
+﻿using System.Collections.Generic;
+
+namespace XepaCommerce.src.repositorios
 {
+    /// <summary>
+    /// <para>Resumo: Responsavel por representar ações de CRUD de usuario</para>
+    /// <para>Criado por: Vinicius Santos Matos | Grupo 04 </para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 05/05/2022</para>
+    /// </summary>
     public interface IUsuario
     {
+        UsuarioModelo PegarUsuarioPeloId(int id);
+        List<UsuarioModelo> PegarUsuariosPeloNome(string nome);
+        UsuarioModelo PegarUsuarioPeloEmail(string email);
+        void NovoUsuario(NovoUsuarioDTO usuario);
+        void AtualizarUsuario(AtualizarUsuarioDTO usuario);
+        void DeletarUsuario(int id);
     }
 }
