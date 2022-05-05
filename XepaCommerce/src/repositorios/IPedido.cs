e0852a62b41c2cbea9ed5fff44c145224c4ebe0f
matheuscms95 <matheuscms95@gmail.com>
2022-05-05 11:13:24 -0300

    @matheuscms95 [Task: Criando IPedido] {05/05/2022 11:13}

---
+++ XepaCommerce/src/repositorios/IPedido.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using XepaCommerce.src.modelos;
+
+namespace XepaCommerce.src.repositorios
+{
+    public interface IPedido
+    {
+        void NovoPedido(NovoPedidoDTO pedido);
+        void DeletarPedido(int id);
+        PedidoModelo PegarPedidoPeloId(int id);
+        List<PedidoModelo> PegarTodosPedidos();
+        List<PedidoModelo> PegarPedidoPorProduto(string produto);
+    }
+}
