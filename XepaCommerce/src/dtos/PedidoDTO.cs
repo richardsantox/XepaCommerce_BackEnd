563d4401f8047f0985c250d16e9b0f9ccc036dd2
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-05-05 12:10:20 -0300

    @Lucas-Reluz [Task: Resolvendo Conflitos V2] {05/05/2022 | 12:10}

--- XepaCommerce/src/dtos/PedidoDTO.cs
+++ XepaCommerce/src/dtos/PedidoDTO.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:XepaCommerce/src/dtos/PedidoDTO.cs
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -41,47 +40,3 @@ namespace XepaCommerce.src.dtos
         }
     }
 }
-=======
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace XepaCommerce.src.dtos
-{
-    /// <summary>
-    /// <para>Resumo: Classe espelho para criar um novo pedido</para>
-    /// <para>Crriado por: Richard Santos</para>
-    /// <para>Versão: 1.0</para>
-    /// <para>Data: 05/05/2022 11:18</para>
-    /// </summary>
-    public class NovoPedidoDTO
-    {
-        [Required]
-        public int Quantidade { get; set; }
-
-        [Required]
-        public float PrecoTotal { get; set; }
-
-        [Required, StringLength(50)]
-        public string StatusPedido { get; set; }
-
-        [Required, StringLength(30)]
-        public string FormaDePagamento { get; set; }
-
-        [Required]
-        public string NomeComprador { get; set; }
-
-        [Required]
-        public string ProdutoComprado { get; set; }
-
-        public NovoPedidoDTO(int quantidade, float precoTotal, string statusPedido, string formaDePagamento, string nomeComprador, string produtoComprado)
-        {
-            Quantidade = quantidade;
-            PrecoTotal = precoTotal;
-            StatusPedido = statusPedido;
-            FormaDePagamento = formaDePagamento;
-            NomeComprador = nomeComprador;
-            ProdutoComprado = produtoComprado;
-        }
-    }
-}
->>>>>>> c44a131f3831a10afeac52f2da570d036e71f986:XepaCommerce/src/dtos/PedidosDTO.cs
--- XepaCommerce/src/repositorios/IProduto.cs
+++ XepaCommerce/src/repositorios/IProduto.cs
@@ -1,9 +1,5 @@
 using System.Collections.Generic;
-<<<<<<< HEAD
 using XepaCommerce.src.dtos;
-=======
-using XepaCommerce.src.dtos;
->>>>>>> c44a131f3831a10afeac52f2da570d036e71f986
 using XepaCommerce.src.modelos;
 
 namespace XepaCommerce.src.repositorios
