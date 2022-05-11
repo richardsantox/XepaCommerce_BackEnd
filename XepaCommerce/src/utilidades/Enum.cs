74ceb4e68263f00d139ed1bb5790afabfd15927c
richardsantox <richardsantospereira4322@gmail.com>
2022-05-11 14:17:13 -0300

    @richardsantox [Task: Craindo Enum e adicionando Secret] {11/05/2022}

---
+++ XepaCommerce/src/utilidades/Enum.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace XepaCommerce.src.utilidades
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TipoUsuario
+    {
+        NORMAL,
+        ADMINISTRADOR
+    }
+}
