a9b9541fad124f6ebf2851160957533f90fea64a
danny-for <dannygeovanna2019@gmail.com>
2022-05-11 16:28:02 -0300

    @Danny-for [Task : Criando AutenticacaoControlador] {11/05/202216:27}

---
+++ XepaCommerce/src/Controladores/AutenticacaoControlador.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using XepaCommerce.src.dtos;
+using XepaCommerce.src.servicos;
+
+namespace XepaCommerce.src.Controladores
+{
+    [ApiController]
+    [Route("api/Autenticacao")]
+    [Produces("application/json")]
+    public class AutenticacaoControlador : ControllerBase
+    {
+
+        #region Atributos
+
+        private readonly IAutenticacao _servicos;
+
+        #endregion
+
+        #region Construtores
+
+        public AutenticacaoControlador(IAutenticacao servicos)
+        {
+            _servicos = servicos;
+        }
+
+        #endregion
+
+        #region Métodos
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Autenticar([FromBody] AutenticarDTO autenticacao)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            try
+            {
+                var autorizacao = _servicos.PegarAutorizacao(autenticacao);
+                return Ok(autorizacao);
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+        #endregion Métodos
+    }
+}
