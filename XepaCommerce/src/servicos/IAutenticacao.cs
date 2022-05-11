9ddd1cbb69b3227ad8faa637d0f113a8b2b0a3ae
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-05-11 15:23:01 -0300

    @Lucas-Reluz [Task: Add interface de autenticacao e implementando] {11/05/2022 | 15:22}

---
+++ XepaCommerce/src/servicos/IAutenticacao.cs
@@ -0,0 +1,16 @@
+﻿using XepaCommerce.src.dtos;
+using XepaCommerce.src.modelos;
+
+namespace XepaCommerce.src.servicos
+{
+    public interface IAutenticacao
+    {
+        string CodificarSenha(string senha);
+        
+        void CriarUsuarioSemDuplicar(NovoUsuarioDTO dto);
+
+        string GerarToken(UsuarioModelo usuario);
+
+        AutorizacaoDTO PegarAutorizacao(AutenticarDTO autenticacao);
+    }
+}
