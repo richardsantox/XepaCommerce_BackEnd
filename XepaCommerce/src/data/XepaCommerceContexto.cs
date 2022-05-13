6dfc929addb00d9198ef60ff39172fac9e208f1f
richardsantox <richardsantospereira4322@gmail.com>
2022-05-13 15:01:43 -0300

    @richardsantox [Task: Criando documentação] {13/05/2022  15:01}

--- XepaCommerce/src/data/XepaCommerceContexto.cs
+++ XepaCommerce/src/data/XepaCommerceContexto.cs
@@ -3,6 +3,12 @@ using XepaCommerce.src.modelos;
 
 namespace XepaCommerce.src.data
 {
+    /// <summary>
+    /// <para>Resumo: Classe contexto, responsável por carregar contexto e definir DbSets</para>
+    /// <para>Criado por: Richard Santos</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 13/05/2022</para>
+    /// </summary>
     public class XepaCommerceContexto : DbContext
     {
         public DbSet<UsuarioModelo> Usuarios { get; set; }
